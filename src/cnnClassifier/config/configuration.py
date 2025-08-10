e31cd7e9283ea1657f562063ea5bcc5b015a98ac
G-Sahil123 <sahil.20222044@mnnit.ac.in>
2025-08-11 03:05:20 +0530

    model evaluated

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,7 +1,7 @@
 from src.cnnClassifier.constants import *
-from src.cnnClassifier.utils.common import read_yaml,create_directories
+from src.cnnClassifier.utils.common import read_yaml,create_directories,save_json
 from src.cnnClassifier.entity.config_entity import (DataIngestionConfig,PrepareBaseModelConfig,
-                                                    TrainingConfig)
+                                                    TrainingConfig,EvaluationConfig)
 import os
 
 class ConfigurationManager:
@@ -70,3 +70,16 @@ class ConfigurationManager:
         )
 
         return training_config    
+    
+
+        
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model=Path("artifacts/training/model.h5"),
+            training_data=Path("artifacts/data_ingestion/CT-KIDNEY-DATASET-Normal-Cyst-Tumor-Stone"),
+            mlflow_uri="https://dagshub.com/G-Sahil123/Kidney_Disease_Classification_DL.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
